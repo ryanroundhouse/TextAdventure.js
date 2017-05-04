b8be99086c604f9cb0b4cfdaee27ecf3f01feb52
Ryan Graham <ryan@graham.pub>
2017-05-03 22:04:21 -0400

    trivial change to trigger openshift deployment

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static(__dirname + '/terminal'));
 app.use(session({secret: '1234567890QWERTY', resave: false, saveUninitialized: true}));
 
 // === Start Server ===
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8000;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || '80';
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 var server = app.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
